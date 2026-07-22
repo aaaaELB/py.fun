6a8ea5581852e94943ea34eab0d7e511ad068e31
ELB <aaaa.elb@gmail.com>
2026-07-22 12:22:25 -0300

    função de criar conta no main

--- func.py
+++ func.py
@@ -14,3 +14,32 @@ def verificar_senha(user, senha):
         if usuario['user'] == user and usuario['senha'] == hash_senha:
             return True
     return False
+
+def criar_conta(user, senha):
+    while True:
+        user = input("Digite o nome do novo usuário: ")
+        with open('D:\\Codes\\py.fun\\usuarios.json', 'r') as f:
+            usuarios = json.load(f)
+        if any(usuario['user'] == user for usuario in usuarios["usuarios"]):
+            print("Usuário já existe. Tente outro nome.")
+        else:
+           break
+
+    while True:
+       senha = input("Digite a senha do novo usuário: ")
+       confirmar_senha = input("Confirme a senha: ")
+       if senha == confirmar_senha:
+           break
+       else:
+           print("As senhas não coincidem. Tente novamente.")
+
+    salt = "brawl"
+    hash_senha = hashlib.sha256((senha + salt).encode('utf-8')).hexdigest()
+
+    with open('D:\\Codes\\py.fun\\usuarios.json', 'r') as f:
+        usuarios = json.load(f)
+
+    usuarios["usuarios"].append({"user": user, "senha": hash_senha})
+
+    with open('D:\\Codes\\py.fun\\usuarios.json', 'w') as f:
+        json.dump(usuarios, f)
--- main.py
+++ main.py
@@ -6,4 +6,8 @@ senha = input("Digite sua senha: ")
 if func.verificar_senha(user, senha):
     print("Login bem-sucedido!\n")
 else:
-    print("Login ou senha incorretos.\n")
+    criar_conta = input("Login ou senha incorretos.\nDeseja criar uma conta? (s/n): ")
+
+if criar_conta.lower() == 's':
+    func.criar_conta(user, senha)
+    print("Conta criada com sucesso!")
